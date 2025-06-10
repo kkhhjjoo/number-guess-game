0b1b5387f0ddc91199d3d3421e9bd7243708c98a
hyunjoo.kim <khjoo1203@gmail.com>
2025-06-10 20:28:04 +0900

    두번째 커밋

--- main.js
+++ main.js
@@ -9,9 +9,62 @@
 //유저가 이미 입력한 숫자를 또 입력하면, 알려준다. 기회를 깍지 않는다
 
 let computerNum = 0;
+let playButton = document.getElementById('play-button');
+let userInput = document.getElementById('user-input');
+let resultArea = document.getElementById('result-area');
+let resetButton = document.getElementById('reset-button');
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById('chance-area');
+let history = [];
+
+playButton.addEventListener('click', play);
+resetButton.addEventListener('click', reset);
+userInput.addEventListener('focus', function () {
+  userInput.value = '';
+});
 
 function pickRandomNum() {
-  computerNum = Math.random();
+  computerNum = Math.floor(Math.random() * 100) + 1;
   console.log('정답', computerNum);
+}
+function play() {
+  let userValue = userInput.value;
+
+  if (userValue < 1 || userValue > 100) {
+    resultArea.textContent = '1과 100사이 숫자를 입력해주세요';
+    return;
+  }
+  if (history.includes(userValue)) {
+    resultArea.textContent =
+      '이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요.';
+    return;
+  }
+  chances--;
+  chanceArea.textContent = `남은기회:${chances}번`;
+  console.log('chance', chances);
+  if (userValue < computerNum) {
+    resultArea.textContent = 'Up!!';
+  } else if (userValue > computerNum) {
+    resultArea.textContent = 'Down!!';
+  } else {
+    resultArea.textContent = '맞추셨습니다!!';
+    gameOver = true;
+  }
+  history.push(userValue);
+  console.log(history);
+  if (chances < 1) {
+    gameOver = true;
+  }
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+function reset() {
+  //user input창이 깨끗하게 정리되고
+  userInput.value = '';
+  //새로운 번호가 생성되고
+  pickRandomNum();
+  resultArea.textContent = '결과가 나온다';
 }
 pickRandomNum();
